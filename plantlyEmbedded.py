20fa6958ec9f79d5474e4cf88353ad713ba71d71
rrudziutki <r.makuchowski1@gmail.com>
2023-09-17 17:27:03 +0200

    added API handling

--- plantlyEmbedded.py
+++ plantlyEmbedded.py
@@ -1,12 +1,46 @@
 from machine import ADC, Pin
+import network
+import urequests
 import utime
+import ujson
+
+api_url = "apiURL"
 
 def get_capacitator_measurements():
-    soil_adc = ADC(Pin(27)) #convert analog signal into digital on pin27
-    max_moisture_value = 65535 #max value of uint16
+    soil_adc = ADC(Pin(27)) # convert analog signal into digital
+    max_moisture_value = 65535 # max value of uint16
+    moistureData = 100 - ((soil_adc.read_u16()*100)/ max_moisture_value) # calculate moisture
+    moisturePercentage = "%.2f" % moistureData
+    print(f"moisture: {moisturePercentage}% (adc: {str(soil_adc.read_u16())})")
+    return moisturePercentage
+
+def connectToInternet(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+def makeGetRequest(url):
+    print("Querying the user object")
+    r = urequests.get(url)
+    print(r.json())
+
+def makePostRequest(data, api_url):
+    data = {
+        "name": "Test",
+        "last_name": "Test",
+        "purpose": data
+    }
+
+    json_data = ujson.dumps(data)
+    response = urequests.post(api_url, data=json_data)
+    response_text = response.text
+    response.close()
+    print("Response:", response_text)
+    print(json_data)
+
+if __name__ == "__main__":
+    connectToInternet("name", "password")
     while True:
-        moisture = 100 - ((soil_adc.read_u16()*100)/ max_moisture_value) #calculate moisture
-        print("moisture: " + "%.2f" % moisture +"% (adc: "+ str(soil_adc.read_u16()) + ")")
-        utime.sleep(0.5)
-        
-get_capacitator_measurements()
+        moistureData = get_capacitator_measurements()
+        makePostRequest(moistureData, api_url)
+        utime.sleep(600)
